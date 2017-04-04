953e7022ffc753ca66e11a06567072666fdd91b9
kevin <929118967@qq.com>
2017-04-04 09:41:36 +0800

    Initial Commit

---
+++ KNWeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNWeChat
+//
+//  Created by devzkn on 04/04/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNWeChat
+//
+//  Created by devzkn on 04/04/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
